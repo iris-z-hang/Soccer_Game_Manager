86db017d3cdf0a7210fd5a27e2658c4c64dd011d
iris_z_hang <iriszhang50@gmail.com>
2023-08-09 11:09:40 -0700

    fixed division

--- otherQueries.php
+++ otherQueries.php
@@ -84,26 +84,6 @@
 
         <hr />
 
-  <!--       <h2>Count the Tuples in Displayed Table</h2>
-        <form method="GET" action="otherQueries.php">
-            <input type="hidden" id="countQueryRequest" name="countQueryRequest">
-						Table: <select name="tName"> 
-                    <option>Player</option>
-                    <option>Team</option>
-                    <option>Club</option>
-					<option>Coach_C1</option>
-                    <option>Coach_C2</option>
-                    <option>Agent</option>
-					<option>Sponsor</option>
-                    <option>Game</option>
-                    <option>Stadium_PNAS</option>
-					<option>Stadium_PCI</option>
-                    <option>Stadium_PCO</option>
-                    <option>Stage</option>
-                  </select> <br /> <br />
-            <input type="submit" value="Count" name="countTuples"></p>
-        </form> -->
-
         <?php
         $success = True; //keep track of errors so it redirects the page only if there are no errors
         $db_conn = NULL; // edit the login credentials in connectToDB()
@@ -139,17 +119,17 @@
                 $success = False;
             }
 
-			return $statement;
-		}
+            return $statement;
+        }
 
         function executeBoundSQL($cmdstr, $list) {
             /* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
-		In this case you don't need to create the statement several times. Bound variables cause a statement to only be
-		parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
-		See the sample code below for how this function is used */
+        In this case you don't need to create the statement several times. Bound variables cause a statement to only be
+        parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
+        See the sample code below for how this function is used */
 
-			global $db_conn, $success;
-			$statement = OCIParse($db_conn, $cmdstr);
+            global $db_conn, $success;
+            $statement = OCIParse($db_conn, $cmdstr);
 
             if (!$statement) {
                 echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
@@ -164,7 +144,7 @@
                     //echo "<br>".$bind."<br>";
                     OCIBindByName($statement, $bind, $val);
                     unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-				}
+                }
 
                 $r = OCIExecute($statement, OCI_DEFAULT);
                 if (!$r) {
@@ -206,8 +186,7 @@
             global $db_conn;
 
             // Your username is ora_(CWL_ID) and the password is a(student number). For example,
-			// ora_platypus is the username and a12345678 is the password.
-            // $db_conn = OCILogon("ora_zhangi1", "a29544764", "dbhost.students.cs.ubc.ca:1522/stu");
+            // ora_platypus is the username and a12345678 is the password.
             $db_conn = OCILogon("ora_zhangi1", "a29544764", "dbhost.students.cs.ubc.ca:1522/stu");
 
 
@@ -287,17 +266,7 @@
         OCICommit($db_conn);
     }
 
-    // function handleCountRequest() {
-    //     global $db_conn;
-
-    //     $result = executePlainSQL("SELECT Count(*) FROM demoTable");
-
-    //     if (($row = oci_fetch_row($result)) != false) {
-    //         echo "<br> The number of tuples in demoTable: " . $row[0] . "<br>";
-    //     }
-    // }
-
-        function handleSelectionRequest() {
+    function handleSelectionRequest() {
         global $db_conn;
 
         $table = $_POST['selectTable'];
@@ -316,9 +285,6 @@
 
         $attribute = $_POST['projectAttribute'];
         $table = $_POST['projectTable'];
-        // $condition = $_POST['projectCond'];
-        // $condition2 = $_POST['projectCond2'];
-        // $condition3 = $_POST['projectCond3'];
         
         $result = executePlainSQL("SELECT " . $attribute . " FROM " . $table . "");
         printTable($result);
@@ -344,14 +310,11 @@
     }
 
     function handleDivisionRequest() {
-
-        $result = executePlainSQL("SELECT T.team_name FROM teamPlaysInGame T, GameIsInStage G WHERE NOT EXISTS ((SELECT T1.GID FROM GameIsInStage T1 WHERE T1.GID=G.GID) EXCEPT (SELECT GID FROM GameIsInStage");
+        $result = executePlainSQL("SELECT DISTINCT T.team_name FROM teamPlaysInGame T WHERE NOT EXISTS (SELECT SID FROM Stage MINUS SELECT G2.SID FROM GameIsInStage G2 WHERE G2.team_name=T.team_name)");
 
         printTable($result);
     }
 
-    // HANDLE ALL POST ROUTES
-	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
     function handlePOSTRequest() {
         if (connectToDB()) {
             if (array_key_exists('selectTableAttribute', $_POST)) {
@@ -368,12 +331,9 @@
             disconnectFromDB();
         }
     }
-        // HANDLE ALL GET ROUTES
-	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
+        
     function handleGETRequest() {
         if (connectToDB()) {
-            // if (array_key_exists('countTuples', $_GET)) {
-            //     handleCountRequest(); }
             if (array_key_exists('displayTuples', $_GET)) {
                 handleShowRequest();
             } 
