99baf4295e650e0597cf7d0f54d4e63332c56291
UBC Student <scenery@students.cs.ubc.ca>
2023-08-08 21:01:39 -0700

    Add main page

---
+++ mainPage.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>Soccer Game Manager</title>
+				<meta charset="UTF-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="styles.css" type="text/css" rel="stylesheet"> 
+    </head>
+
+    <body>
+			<div class="container">
+				<div class="top-section" >
+					<h1>Soccer Game Manager</h1>
+				</div>
+
+				<div class="football-image" >
+				  <img src="football.png">
+				</div>
+
+				<div class="flexbox">
+					 <div class="left-section">
+						<a href="basicQueries.php"><p>Basic Queries</p></a>
+					 </div>
+
+					 <div class="middle-section">
+					  <a href="aggregations.php"><p>Aggregations Queries</p></a>
+					 </div>
+					
+					 <div class="right-section">
+					  <a href="otherQueries.php"><p>Other Queries</p></a>
+					 </div>
+				</div>
+			</div>
+	</body>
+</html>
